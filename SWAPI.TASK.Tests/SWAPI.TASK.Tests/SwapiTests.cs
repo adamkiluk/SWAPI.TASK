3e7f70c0fc207597bd316a2d8593bb844acb812a
Adam Kiluk <adam.kiluk1@gmail.com>
2021-01-18 15:00:27 +0100

    added Database migrations

---
+++ SWAPI.TASK.Tests/SWAPI.TASK.Tests/SwapiTests.cs
@@ -0,0 +1,11 @@
+﻿using SWAPI.TASK.WebUI.Controllers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SWAPI.TASK.Tests
+{
+    public class SwapiTests
+    {
+       
+    }
+}
---
+++ SWAPI.TASK/SWAPI.TASK.WebUI/Migrations/20210118135136_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SWAPI.TASK.WebUI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserReviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EpisodeId = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserRating = table.Column<int>(type: "int", nullable: false),
+                    VoteDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserReviews", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserReviews");
+        }
+    }
+}
